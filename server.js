cc351300410efa22851b506d7a44717b5e0823c5
heeneth <heeneth123@gmail.com>
2023-12-26 12:57:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const { Socket } = require("dgram");
+const { send } = require("process");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "post"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`user connected ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`user disconnected ${socket.id}`);
+    // Perform any necessary cleanup or logic here
+    io.emit("user_disconnected", { message: "disconnected" });
+  });
+});
+
+server.listen(9000, () => {
+  console.log("server is running in 9000");
+});

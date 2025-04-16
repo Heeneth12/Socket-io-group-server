a1e97d9e0e1e41cf75af32551709af47bc95215f
Heeneth sai <114326514+Heeneth12@users.noreply.github.com>
2025-04-16 19:11:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://findme-g66u.vercel.app",
+    origin: "https://ez-meet.vercel.app",
     methods: ["GET", "POST"],
   },
 });
